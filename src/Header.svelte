e98e387babc856d541e2a541208bd0d9540c57e5
Connor Easton <eastonco@icloud.com>
2021-10-10 19:27:25 -0700

    mostly done

---
+++ src/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { username, user} from './user'
+
+    function signout() {
+        user.leave()
+        username.set('')
+    }
+</script>
+
+
+<header>
+    <h1>Chit Chat</h1>
+    {#if $username}
+        <div class="user-bio">
+            <span>Hello <strong>{$username}</strong></span>
+            <img src={`https://avatars.dicebear.com/api/identicon/${$username}.svg`} alt="avatar"/>
+        </div>
+
+        <button class="signout-button" on:click={signout}>Sign out</button>
+    
+    {:else}
+        <h3>Powered by Gun.js & Svelte</h3>
+    {/if}
+</header>
---
+++ src/Login.svelte
@@ -0,0 +1,32 @@
+<script>
+    import {user} from './user.js';
+
+    let username;
+    let password;
+
+    function login() {
+        user.auth(username, password, ({err}) => err && alert(err));
+    }
+
+    function signup() {
+        user.create(username, password, ({err}) => {
+            if(err) {
+                alert(err);
+            } else {
+                login();
+            }
+        });
+    }
+
+</script>
+
+<div class="loginbox">
+    <label for="username">Username:</label>
+    <input class="inputs" name="username" bind:value={username} minlength="3" maxlength="20" />
+
+    <label for="password">Password:</label>
+    <input class="inputs" name="password" bind:value={password} minlength="3" maxlength="20" />
+
+    <button class="login" on:click={login}>Login</button>
+    <button class="login" on:click={signup}>Sign Up</button>
+</div>
